f54d5c18944afd7ac85883f62aba1fedaaa423eb
Zachary Sluder <zachary.sluder@gmail.com>
2018-06-11 08:46:34 -0400

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -13,6 +13,7 @@ return [
     | Supported: "pusher", "redis", "log", "null"
     |
     */
+    
     'default' => env('BROADCAST_DRIVER', 'null'),
 
     /*
@@ -25,6 +26,7 @@ return [
     | each available type of connection are provided inside this array.
     |
     */
+    
     'connections' => [
         'pusher' => [
             'driver' => 'pusher',
